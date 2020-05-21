d573003ce63c175ca14eba3b02ca698593e7bb7b
kayo289 <kayokapi@icloud.com>
2020-05-22 00:40:37 +0900

    login機能の実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def signupfunc(request):
     user2 = User.objects.all()
-    print(user2)
     if request.method == 'POST':
         username2 = request.POST['username']
         password2 = request.POST['password']
@@ -15,3 +15,16 @@ def signupfunc(request):
             user = User.objects.create_user(username2, '', password2)
             return render(request, 'signup.html', {'some': 100})
     return render(request, 'signup.html', {'some': 100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        print(request.POST['username'])
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+    return render(request,'login.html')
